0e1fa3b236f3bacac5761d24588596ad9eae0004
dev <adriana.bispo283@gmail.com>
2022-12-04 20:59:07 -0300

    chama da classe game organizada

--- jogo_da_velha/bin/jogo_da_velha.dart
+++ jogo_da_velha/bin/jogo_da_velha.dart
@@ -1,15 +1,26 @@
-import 'package:jogo_da_velha/menu.dart';
 import 'dart:io';
+import 'package:jogo_da_velha/menu.dart';
+//models
+import 'package:jogo_da_velha/models/game.dart';
+
 void main(List<String> arguments) {
-  bool start = false;
+  Tictactoe game = Tictactoe();
+
   print("Você quer começar o jogo? y/n >> ");
   String? resposta = stdin.readLineSync();
-  if (resposta!.contains('y')){
-    start = true;
-  }else{
+  if (resposta!.contains('y')) {
+    game.start();
+    for (int i = 0; i < 5; i++) {
+      game.jogada();
+      if (game.winner()) {
+        parabens(nomeplayer: game.ganhador);
+        break;
+      } else if (i == 4) {
+        todosPerderam();
+        bye();
+      }
+    }
+  } else {
     bye();
-  }
-  while (start){
-    viewMenu();
   }
 }
